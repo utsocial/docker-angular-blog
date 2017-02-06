149e292199aa1806c08e91dff02f1d505d5b5ad0
ivanchenoweth@gmailcom <ivanchenoweth@gmailcom>
2017-02-05 18:10:43 -0700

    first commit only node

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  console.log("Changes on the fly, from index.js");
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+*/
+/////////////////////////////
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
